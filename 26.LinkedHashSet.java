8970d170f4208162a7232a8be8377485a988599b
cjita <147354799+cjita@users.noreply.github.com>
2025-03-30 21:49:06 +0530

    Create 26.LinkedHashSet.java

---
+++ 26.LinkedHashSet.java
@@ -0,0 +1,23 @@
+import java.util.Set;
+import java.util.LinkedHashSet;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Set<Integer> set=new LinkedHashSet<>();
+		
+		set.add(25);
+		set.add(85);
+		set.add(32);
+		set.add(48);
+		System.out.println(set);
+		set.remove(32);
+		System.out.println(set);
+		System.out.println(set.contains(25));
+		System.out.println(set.isEmpty());
+		System.out.println(set.size());
+		set.clear();
+        System.out.println(set);
+
+	}
+}
